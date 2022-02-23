74cbbb114c98b0a0f6ea0d283c6748b27e40427c
Tobias Hienzsch <tobias@modern-circuits.com>
2022-02-23 04:39:15 +0100

    Add Catch2 tests

---
+++ Source/mc/RaumAkustik/FirstReflection.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+namespace mc
+{
+
+struct Point
+{
+    double x {0};
+    double y {0};
+    double z {0};
+};
+
+using Speaker = Point;
+
+struct RoomDimensions
+{
+    double length {0};
+    double width {0};
+    double height {0};
+};
+
+struct RoomLayout
+{
+    RoomDimensions dimensions {};
+    std::array<Speaker, 2> speakers {};
+    Point listenPosition {};
+};
+
+struct FirstReflection
+{
+    double front {0};
+    double left {0};
+    double right {0};
+};
+
+namespace detail
+{
+inline auto firstReflectionSideWall(RoomDimensions dimensions, Point listenPosition, Speaker speaker, double x1)
+    -> double
+{
+    auto const y  = listenPosition.y - speaker.y;
+    auto const x2 = dimensions.width - listenPosition.x;
+    return listenPosition.y - (y * x2) / (x1 + x2);
+}
+
+inline auto firstReflectionFrontWall(RoomDimensions dimensions, Point listenPosition, Speaker speaker) -> double
+{
+    auto const y  = std::abs(speaker.x - listenPosition.x);
+    auto const x1 = speaker.y;
+    auto const x2 = listenPosition.y;
+    return listenPosition.x - (y * x2) / (x1 + x2);
+}
+
+}  // namespace detail
+
+template<typename InIt, typename OutIt>
+auto firstReflections(InIt f, InIt l, OutIt o, RoomDimensions dimensions, Point listenPosition) -> void
+{
+    while (f != l)
+    {
+        *o = FirstReflection {
+            detail::firstReflectionFrontWall(dimensions, listenPosition, *f),
+            detail::firstReflectionSideWall(dimensions, listenPosition, *f, f->x),
+            detail::firstReflectionSideWall(dimensions, listenPosition, *f, dimensions.width - f->x),
+        };
+        ++f;
+        ++o;
+    }
+}
+
+}  // namespace mc
---
+++ Source/mc/RaumAkustik/FirstReflection.test.cpp
@@ -0,0 +1,43 @@
+#include "FirstReflection.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+static constexpr auto room = mc::RoomLayout {
+    mc::RoomDimensions {
+        600,
+        365,
+        312,
+    },
+    {
+        mc::Speaker {
+            121,
+            160,
+            125,
+        },
+        mc::Speaker {
+            121 + 123,
+            160,
+            125,
+        },
+    },
+    mc::Point {
+        121 + 123 / 2.0,
+        160 + 123,
+        120,
+    },
+};
+
+TEST_CASE("RaumAkustik: firstReflections", "[RaumAkustik]")
+{
+    auto reflections = std::array<mc::FirstReflection, 2> {};
+    mc::firstReflections(begin(room.speakers), end(room.speakers), begin(reflections), room.dimensions,
+                         room.listenPosition);
+
+    REQUIRE(static_cast<int>(reflections[0].front) == 143);
+    REQUIRE(static_cast<int>(reflections[0].left) == 209);
+    REQUIRE(static_cast<int>(reflections[0].right) == 230);
+
+    REQUIRE(static_cast<int>(reflections[1].front) == 143);
+    REQUIRE(static_cast<int>(reflections[1].right) == 209);
+    REQUIRE(static_cast<int>(reflections[1].left) == 230);
+}
