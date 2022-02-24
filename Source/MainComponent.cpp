1b790505d6bd5190f68a0a72c1b60f4bea6becd4
Tobias Hienzsch <tobias@modern-circuits.com>
2022-02-24 23:26:39 +0100

    Add keyboard shortcuts

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,21 +2,12 @@
 
 MainComponent::MainComponent()
 {
-    addAndMakeVisible(_undo);
-    addAndMakeVisible(_redo);
-    addAndMakeVisible(_load);
-    addAndMakeVisible(_save);
+    addAndMakeVisible(_menuBar);
     addAndMakeVisible(_tabs);
 
-    _undo.onClick = [this] { _undoManager.undo(); };
-    _redo.onClick = [this] { _undoManager.redo(); };
-    _save.onClick = [this] { saveSession(); };
-    _load.onClick = [this] { loadSession(); };
-
-    _undo.setImages(juce::Drawable::createFromImageData(mcbd::undo_svg, mcbd::undo_svgSize).get());
-    _redo.setImages(juce::Drawable::createFromImageData(mcbd::redo_svg, mcbd::redo_svgSize).get());
-    _save.setImages(juce::Drawable::createFromImageData(mcbd::save_svg, mcbd::save_svgSize).get());
-    _load.setImages(juce::Drawable::createFromImageData(mcbd::folder_open_svg, mcbd::folder_open_svgSize).get());
+    _commandManager.registerAllCommandsForTarget(this);
+    addKeyListener(_commandManager.getKeyMappings());
+    setWantsKeyboardFocus(true);
 
     reloadUI();
     setSize(1280, 720);
@@ -31,16 +22,56 @@ auto MainComponent::paint(juce::Graphics& g) -> void
 
 auto MainComponent::resized() -> void
 {
-    auto area = getLocalBounds().reduced(10);
+    auto area = getLocalBounds();
+    _menuBar.setBounds(area.removeFromTop(juce::LookAndFeel::getDefaultLookAndFeel().getDefaultMenuBarHeight()));
+    _tabs.setBounds(area.reduced(10));
+}
 
-    auto buttonArea        = area.removeFromTop(area.proportionOfHeight(0.1));
-    auto const buttonWidth = buttonArea.proportionOfWidth(0.1);
-    _undo.setBounds(buttonArea.removeFromLeft(buttonWidth).reduced(5));
-    _redo.setBounds(buttonArea.removeFromLeft(buttonWidth).reduced(5));
-    _load.setBounds(buttonArea.removeFromLeft(buttonWidth).reduced(5));
-    _save.setBounds(buttonArea.removeFromLeft(buttonWidth).reduced(5));
+auto MainComponent::getNextCommandTarget() -> juce::ApplicationCommandTarget*
+{
+    return findFirstTargetParentComponent();
+}
 
-    _tabs.setBounds(area);
+auto MainComponent::getAllCommands(juce::Array<juce::CommandID>& c) -> void
+{
+    c.addArray({CommandIDs::redo, CommandIDs::undo, CommandIDs::open, CommandIDs::save});
+}
+
+auto MainComponent::getCommandInfo(juce::CommandID commandID, juce::ApplicationCommandInfo& result) -> void
+{
+    switch (commandID)
+    {
+        case CommandIDs::open:
+            result.setInfo("Open", "Opens a session file", "File", 0);
+            result.addDefaultKeypress('o', juce::ModifierKeys::commandModifier);
+            break;
+        case CommandIDs::save:
+            result.setInfo("Save", "Saves a session file", "File", 0);
+            result.addDefaultKeypress('s', juce::ModifierKeys::commandModifier);
+            break;
+        case CommandIDs::undo:
+            result.setInfo("Undo", "Undo one step", "Edit", 0);
+            result.addDefaultKeypress('z', juce::ModifierKeys::commandModifier);
+            break;
+        case CommandIDs::redo:
+            result.setInfo("Redo", "Redo one step", "Edit", 0);
+            result.addDefaultKeypress('z', juce::ModifierKeys::commandModifier | juce::ModifierKeys::shiftModifier);
+            break;
+        default: break;
+    }
+}
+auto MainComponent::perform(juce::ApplicationCommandTarget::InvocationInfo const& info) -> bool
+{
+    switch (info.commandID)
+    {
+        case CommandIDs::open: loadSession(); break;
+        case CommandIDs::save: saveSession(); break;
+        case CommandIDs::undo: _undoManager.undo(); break;
+        case CommandIDs::redo: _undoManager.redo(); break;
+        default: return false;
+    }
+
+    return true;
 }
 
 auto MainComponent::saveSession() -> void
--- Source/MainComponent.hpp
+++ Source/MainComponent.hpp
@@ -5,7 +5,72 @@
 
 #include <juce_gui_extra/juce_gui_extra.h>
 
-struct MainComponent final : juce::Component
+struct CommandIDs
+{
+    enum Values
+    {
+        unknown,
+        undo,
+        redo,
+        open,
+        save,
+    };
+};
+
+struct MenuBar final
+    : juce::Component
+    , juce::MenuBarModel
+{
+    MenuBar(juce::ApplicationCommandManager& commandManager) : _commandManager {commandManager}
+    {
+        setApplicationCommandManagerToWatch(&_commandManager);
+        addAndMakeVisible(_menu);
+    }
+
+    ~MenuBar() override = default;
+
+    auto resized() -> void override { _menu.setBounds(getLocalBounds()); }
+
+    juce::StringArray getMenuBarNames() override { return {"File", "Edit"}; }
+
+    juce::PopupMenu getMenuForIndex(int menuIndex, const juce::String& /*menuName*/) override
+    {
+        juce::PopupMenu menu;
+
+        if (menuIndex == 0)
+        {
+            auto openIcon = juce::Drawable::createFromImageData(mcbd::folder_open_svg, mcbd::folder_open_svgSize);
+            auto saveIcon = juce::Drawable::createFromImageData(mcbd::save_svg, mcbd::save_svgSize);
+            menu.addCommandItem(&_commandManager, CommandIDs::open, {}, std::move(openIcon));
+            menu.addCommandItem(&_commandManager, CommandIDs::save, {}, std::move(saveIcon));
+        }
+        else if (menuIndex == 1)
+        {
+            auto undoIcon = juce::Drawable::createFromImageData(mcbd::undo_svg, mcbd::undo_svgSize);
+            auto redoIcon = juce::Drawable::createFromImageData(mcbd::redo_svg, mcbd::redo_svgSize);
+            menu.addCommandItem(&_commandManager, CommandIDs::undo, {}, std::move(undoIcon));
+            menu.addCommandItem(&_commandManager, CommandIDs::redo, {}, std::move(redoIcon));
+        }
+        // else if (menuIndex == 2)
+        // {
+        //     menu.addCommandItem (&_commandManager, CommandIDs::innerColourRed);
+        //     menu.addCommandItem (&_commandManager, CommandIDs::innerColourGreen);
+        //     menu.addCommandItem (&_commandManager, CommandIDs::innerColourBlue);
+        // }
+
+        return menu;
+    }
+
+    void menuItemSelected(int /*menuItemID*/, int /*topLevelMenuIndex*/) override { }
+
+private:
+    juce::ApplicationCommandManager& _commandManager;
+    juce::MenuBarComponent _menu {this};
+};
+
+struct MainComponent final
+    : juce::Component
+    , juce::ApplicationCommandTarget
 {
     MainComponent();
     ~MainComponent() override;
@@ -13,18 +78,21 @@ struct MainComponent final : juce::Component
     void paint(juce::Graphics&) override;
     void resized() override;
 
+    auto getNextCommandTarget() -> juce::ApplicationCommandTarget*;
+    auto getAllCommands(juce::Array<juce::CommandID>& commands) -> void;
+    auto getCommandInfo(juce::CommandID commandID, juce::ApplicationCommandInfo& result) -> void;
+    auto perform(juce::ApplicationCommandTarget::InvocationInfo const& info) -> bool;
+
 private:
     auto saveSession() -> void;
     auto loadSession() -> void;
     auto reloadUI() -> void;
 
+    juce::ApplicationCommandManager _commandManager {};
     juce::UndoManager _undoManager;
     juce::ValueTree _valueTree {"RaumAkustik"};
 
-    juce::DrawableButton _undo {"Undo", juce::DrawableButton::ImageFitted};
-    juce::DrawableButton _redo {"Redo", juce::DrawableButton::ImageFitted};
-    juce::DrawableButton _load {"Load", juce::DrawableButton::ImageFitted};
-    juce::DrawableButton _save {"Save", juce::DrawableButton::ImageFitted};
+    MenuBar _menuBar {_commandManager};
 
     juce::TabbedComponent _tabs {juce::TabbedButtonBar::TabsAtTop};
     std::unique_ptr<mc::FirstReflectionsView> _firstReflectionsView {};
