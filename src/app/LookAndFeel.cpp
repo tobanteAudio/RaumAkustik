4092ded3c3b44d6ec4687529de7f3057656bdad2
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-06-01 17:31:18 +0200

    Update actions/checkout to v3

--- src/app/LookAndFeel.cpp
+++ src/app/LookAndFeel.cpp
@@ -3,17 +3,17 @@
 namespace mc
 {
 LookAndFeel::LookAndFeel()
-    : juce::LookAndFeel_V4{{
-        juce::Colour::fromRGB(30, 41, 59),     // windowBackground
-        juce::Colour::fromRGB(51, 65, 85),     // widgetBackground
-        juce::Colour::fromRGB(51, 65, 85),     // menuBackground
-        juce::Colours::transparentBlack,       // outline
-        juce::Colour::fromRGB(248, 250, 252),  // defaultText
-        juce::Colour::fromRGB(15, 23, 42),     // defaultFill
-        juce::Colour::fromRGB(248, 250, 252),  // highlightedText
-        juce::Colour::fromRGB(234, 88, 12),    // highlightedFill
-        juce::Colour::fromRGB(248, 250, 252),  // menuText
-    }}
+    // : juce::LookAndFeel_V4{{
+    //     juce::Colour::fromRGB(30, 41, 59),     // windowBackground
+    //     juce::Colour::fromRGB(51, 65, 85),     // widgetBackground
+    //     juce::Colour::fromRGB(51, 65, 85),     // menuBackground
+    //     juce::Colours::transparentBlack,       // outline
+    //     juce::Colour::fromRGB(248, 250, 252),  // defaultText
+    //     juce::Colour::fromRGB(15, 23, 42),     // defaultFill
+    //     juce::Colour::fromRGB(248, 250, 252),  // highlightedText
+    //     juce::Colour::fromRGB(234, 88, 12),    // highlightedFill
+    //     juce::Colour::fromRGB(248, 250, 252),  // menuText
+    // }}
 {
     setColour(juce::TabbedButtonBar::tabOutlineColourId, juce::Colours::transparentBlack);
 }
