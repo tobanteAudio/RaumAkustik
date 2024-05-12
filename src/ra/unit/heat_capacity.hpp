71065a1c9a5fad85d159d3fd567e9e51235568b2
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-05-12 13:18:35 +0200

    Remove JoulePerKilogramKelvin alias

--- src/ra/unit/heat_capacity.hpp
+++ src/ra/unit/heat_capacity.hpp
@@ -6,12 +6,9 @@ namespace ra {
 
 using namespace units::isq;
 
-template<typename T>
-using JoulePerKilogramKelvin = si::specific_heat_capacity<si::joule_per_kilogram_kelvin, T>;
-
 inline constexpr auto SpecificHeatRatio = 1.402;
 
 template<typename T>
-inline constexpr auto GasConstant = JoulePerKilogramKelvin<T>{287.05};
+inline constexpr auto GasConstant = si::specific_heat_capacity<si::joule_per_kilogram_kelvin, T>{287.05};
 
 }  // namespace ra
